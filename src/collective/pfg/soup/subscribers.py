b1390623b75bec6cc24a4dfc2a98aa1bc34e0c6d
Jens W. Klein <jens@bluedynamics.com>
2012-08-28 12:59:50 +0200

    prepare rebuild on add and remove of fields

--- src/collective/pfg/soup/subscribers.py
+++ src/collective/pfg/soup/subscribers.py
@@ -5,3 +5,12 @@ from .storage import PfgCatalogFactory
 def create_catalogfactory(obj, event):
     sm = getSite().getSiteManager()
     sm.registerUtility(factory=PfgCatalogFactory, name=obj._soup_name)
+
+
+def rebuild_catalog(obj, event):
+
+    # check if adapter is present
+    # if not return
+    # get soup
+    # soup.rebuild()
+    pass
