75837d48ac4d5ea7c6b4c147272de2bcd98967f6
Jens W. Klein <jens@bluedynamics.com>
2012-08-28 15:01:40 +0200

    rebuild on IObjectRemovedEvent

--- src/collective/pfg/soup/subscribers.py
+++ src/collective/pfg/soup/subscribers.py
@@ -1,4 +1,7 @@
 from zope.app.component.hooks import getSite
+from Acquisition import aq_parent
+from Products.PloneFormGen.interfaces import IPloneFormGenForm
+from .interfaces import IPfgSoupAdapter
 from .storage import PfgCatalogFactory
 
 
@@ -8,9 +11,19 @@ def create_catalogfactory(obj, event):
 
 
 def rebuild_catalog(obj, event):
-
-    # check if adapter is present
-    # if not return
-    # get soup
-    # soup.rebuild()
-    pass
+    parent = aq_parent(obj)
+    while True:
+        if not parent or IPloneFormGenForm.providedBy(parent):
+            break
+        parent = aq_parent(parent)
+    if parent is None:
+        return
+    sub = None
+    for name in parent.contentIds():
+        sub = parent[name]
+        if IPfgSoupAdapter.providedBy(sub):
+            break
+    if sub is None:
+        return
+    soup = sub.get_soup()
+    soup.rebuild()
